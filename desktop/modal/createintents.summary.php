3fc758a4e22bb47538d9e07c05488925b5cc2dbb
vampi62 <vadidi62@gmail.com>
2024-05-16 20:01:14 +0200

    select intent dans le formulaire

--- desktop/modal/createintents.summary.php
+++ desktop/modal/createintents.summary.php
@@ -6,16 +6,25 @@ $_objet = array();
 foreach ((jeeObject::all()) as $object) {
 	$_objet[$object->getId()] = $object->getName();
 }
+$_plugin = array();
+foreach (plugin::listPlugin(true) as $plugin) {
+	$_plugin[$plugin->getId()] = $plugin->getName();
+}
 // charge le fichier json "formFR.json" qui contient les formulaires
 $filename = __DIR__ . '/formFR.json';
 $json = file_get_contents($filename);
 $configformulaire = json_decode($json, true);
-$_saveFormulaire = config::byKeys(['formulaire-jeedomLumiere','formulaire-jeedomVolet'], 'jeerhasspy');
+$_saveFormulaire = array();
+for ($key = 0; $key < count($configformulaire); $key++) {// charge les données sauvegardées
+	$_saveFormulaire[] = $configformulaire[$key]["idForm"];
+}
+$_saveFormulaireData = config::byKeys($_saveFormulaire, 'jeerhasspy');
 
 $varList = array();
 $varList['_objet'] = $_objet;
+$varList['_plugin'] = $_plugin;
 sendVarToJS('configformulaire', $configformulaire);
-sendVarToJS('_saveFormulaire', $_saveFormulaire);
+sendVarToJS('_saveFormulaire', $_saveFormulaireData);
 sendVarToJS('varList', $varList);
 ?>
 
@@ -38,16 +47,19 @@ sendVarToJS('varList', $varList);
 	console.log(varList)
 	for (key in configformulaire) {
 		// ajout de l'intent dans l'index a	gauche
+		let _frame = document.createElement('div');
+		_frame.id = configformulaire[key]["idForm"];
+		_frame.style.display = 'none';
 		let _menuIntent = document.createElement('div');
 		_menuIntent.style.display = 'flex';
 		_menuIntent.style.justifyContent = 'space-between';
 		_menuIntent.innerHTML = '<a href="#" onclick="showForm(\'' + configformulaire[key]["idForm"] + '\')">' + configformulaire[key]["name"] + '</a>';
 		hasSave = false;
 		if (configformulaire[key]["idForm"] in _saveFormulaire) {
-			hasSave = true;
+			//hasSave = true;
 		}
-		if ((hasSave) && ('active' in _saveFormulaire['formulaire-' + configformulaire[key]["idForm"]])) {
-			if ($_saveFormulaire['formulaire-' + configformulaire[key]["idForm"]]['active'] == "1") {
+		if ((hasSave) && ('active' in _saveFormulaire[configformulaire[key]["idForm"]])) {
+			if (_saveFormulaire[configformulaire[key]["idForm"]]['active'] == "1") {
 				_menuIntent.innerHTML += '<input type="checkbox" id="select' + configformulaire[key]["idForm"] + '" name="' + configformulaire[key]["name"] + '" checked>';
 			} else {
 				_menuIntent.innerHTML += '<input type="checkbox" id="select' + configformulaire[key]["idForm"] + '" name="' + configformulaire[key]["name"] + '">';
@@ -57,10 +69,24 @@ sendVarToJS('varList', $varList);
 		}
 		document.getElementById('indexIntent').appendChild(_menuIntent);
 
+		let menu = document.createElement('div');
+		menu.id = "menu-" + configformulaire[key]["idForm"];
+		menu.style.display = 'flex';
+		menu.style.justifyContent = 'flex-start';
+		let _divmodule = document.createElement('div');
+		_divmodule.innerHTML = '<a href="#" style="margin-left: 5px; onclick="showPanel(\'' + configformulaire[key]["idForm"] + '\',\'module\')">Module</a>';
+		menu.appendChild(_divmodule);
+		let _divphrase = document.createElement('div');
+		_divphrase.innerHTML = '<a href="#" style="margin-left: 5px; onclick="showPanel(\'' + configformulaire[key]["idForm"] + '\',\'phrase\')">Phrase</a>';
+		menu.appendChild(_divphrase);
+		_frame.appendChild(menu);
+
+
+
 		// ajout du formulaire
-		form = document.createElement('div');
-		form.id = configformulaire[key]["idForm"];
-		form.style.display = 'none';
+		let form = document.createElement('div');
+		form.id = "module-" + configformulaire[key]["idForm"];
+		form.classList.add('page');
 		for (configModule in configformulaire[key]["module"]) {
 			formModule = document.createElement('div');
 			formModule.id = configformulaire[key]["idForm"] + "_" + configformulaire[key]["module"][configModule]["name"]
@@ -90,10 +116,10 @@ sendVarToJS('varList', $varList);
 						_option.value = keyOP;
 						_option.text = value;
 						if (hasSave) {
-							for (let i = 0; i < _saveFormulaire['formulaire-' + configformulaire[key]["idForm"]]['intents'].length; i++) {
-								if (_saveFormulaire['formulaire-' + configformulaire[key]["idForm"]]['intents'][i]['name'] == configformulaire[key]["module"][configModule]["name"]) {
-									for (let j = 0; j < _saveFormulaire['formulaire-' + configformulaire[key]["idForm"]]['intents'][i]['value'].length; j++) {
-										if (_saveFormulaire['formulaire-' + configformulaire[key]["idForm"]]['intents'][i]['value'][j] == keyOP) {
+							for (let i = 0; i < _saveFormulaire[configformulaire[key]["idForm"]]['intents'].length; i++) {
+								if (_saveFormulaire[configformulaire[key]["idForm"]]['intents'][i]['name'] == configformulaire[key]["module"][configModule]["name"]) {
+									for (let j = 0; j < _saveFormulaire[configformulaire[key]["idForm"]]['intents'][i]['value'].length; j++) {
+										if (_saveFormulaire[configformulaire[key]["idForm"]]['intents'][i]['value'][j] == keyOP) {
 											_option.selected = true;
 										}
 									}
@@ -112,9 +138,9 @@ sendVarToJS('varList', $varList);
 					_checkbox.type = "checkbox";
 					formModule.appendChild(_checkbox);
 					if (hasSave) {
-						for (let i = 0; i < _saveFormulaire['formulaire-' + configformulaire[key]["idForm"]]['intents'].length; i++) {
-							if (_saveFormulaire['formulaire-' + configformulaire[key]["idForm"]]['intents'][i]['name'] == configformulaire[key]["module"][configModule]["name"]) {
-								_checkbox.checked = _saveFormulaire['formulaire-' + configformulaire[key]["idForm"]]['intents'][i]['value'];
+						for (let i = 0; i < _saveFormulaire[configformulaire[key]["idForm"]]['intents'].length; i++) {
+							if (_saveFormulaire[configformulaire[key]["idForm"]]['intents'][i]['name'] == configformulaire[key]["module"][configModule]["name"]) {
+								_checkbox.checked = _saveFormulaire[configformulaire[key]["idForm"]]['intents'][i]['value'];
 							}
 						}
 					} else if (configformulaire[key]["module"][configModule]["defaultValue"]) {
@@ -126,8 +152,8 @@ sendVarToJS('varList', $varList);
 					_div.style = "display: flex; flex-direction: column;";
 					_div.id = configformulaire[key]["idForm"] + configformulaire[key]["module"][configModule]["name"] + "_multiTexte";
 					if (hasSave) {
-						for (let i = 0; i < _saveFormulaire['formulaire-' + configformulaire[key]["idForm"]]['intents'].length; i++) {
-							if (_saveFormulaire['formulaire-' + configformulaire[key]["idForm"]]['intents'][i]['name'] == configformulaire[key]["module"][configModule]["name"]) {
+						for (let i = 0; i < _saveFormulaire[configformulaire[key]["idForm"]]['intents'].length; i++) {
+							if (_saveFormulaire[configformulaire[key]["idForm"]]['intents'][i]['name'] == configformulaire[key]["module"][configModule]["name"]) {
 								// cut the string with the separator ";"
 								let _value = _saveFormulaire['formulaire-' + configformulaire[key]["idForm"]]['intents'][i]['value'].split(";");
 								for (let j = 0; j < _value.length; j++) {
@@ -158,9 +184,97 @@ sendVarToJS('varList', $varList);
 					formModule.appendChild(_div);
 				break;
 			}
+			if (isset(configformulaire[key]["module"][configModule]["required"])) {
+				permit = false;
+				for (let i = 0; i < configformulaire[key]["module"][configModule]["required"].length; i++) {
+					permit = false;
+					if (isset(configformulaire[key]["module"][configModule]["required"][i]["plugin"])) {
+						for (let j = 0; j < varList['_plugin'].length; j++) {
+							if (configformulaire[key]["module"][configModule]["required"][i]["plugin"] == varList['_plugin'][j]) {
+								permit = true;
+								break;
+							}
+						}
+					}
+					if (!permit) {
+						break;
+					}
+				}
+				//if (!permit) {
+				if (false) {
+					// disable les inputs du bloc et ajoute un masque
+					formModule.style.pointerEvents = 'none';
+					formModule.style.opacity = '0.5';
+					let _mask = document.createElement('div');
+					_mask.style.position = 'absolute';
+					_mask.style.width = '100%';
+					_mask.style.height = '100%';
+					_mask.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+					_mask.style.zIndex = '1';
+					_mask.innerHTML = 'plugin requis non installé'
+					formModule.appendChild(_mask);
+					// find input and disable
+					let _inputs = formModule.querySelectorAll('input');
+					for (let i = 0; i < _inputs.length; i++) {
+						_inputs[i].disabled = true;
+					}
+				}
+			}
 			form.appendChild(formModule);
 		}
-		document.getElementById('pageIntent').appendChild(form);
+		_frame.appendChild(form);
+
+		let phase = document.createElement('div');
+		phase.id = "phrase-" + configformulaire[key]["idForm"];
+		phase.classList.add('page');
+		phase.style.display = 'none';
+
+		let _table = document.createElement('table');
+		let _tr = document.createElement('tr');
+		let _th = document.createElement('th');
+		_th.innerHTML = "activer";
+		_th.style.textAlign = "center";
+		_tr.appendChild(_th);
+		_th = document.createElement('th');
+		_th.innerHTML = "intent";
+		_th.style.textAlign = "center";
+		_tr.appendChild(_th);
+		_th = document.createElement('th');
+		_th.innerHTML = "message de confirmation";
+		_th.style.textAlign = "center";
+		_tr.appendChild(_th);
+		_table.appendChild(_tr);
+		for (configPhrase in configformulaire[key]["phrase"]) {
+			let _tr = document.createElement('tr');
+			let _td = document.createElement('td');
+			let _checkbox = document.createElement('input');
+			_checkbox.type = "checkbox";
+			_checkbox.id = configformulaire[key]["idForm"] + "_phrase_" + configPhrase;
+			_checkbox.checked = 
+			_td.appendChild(_checkbox);
+			_tr.appendChild(_td);
+			_td = document.createElement('td');
+			let _txtintent = document.createElement('input');
+			_txtintent.value = configformulaire[key]["phrase"][configPhrase]["intent"];
+			_txtintent.style.padding = "5px";
+			_td.appendChild(_txtintent);
+			_tr.appendChild(_td);
+			_td = document.createElement('td');
+			let _txtconfirm = document.createElement('input');
+			_txtconfirm.value = configformulaire[key]["phrase"][configPhrase]["confirmMSG"];
+			_txtconfirm.style.padding = "5px";
+			_td.appendChild(_txtconfirm);
+			_tr.appendChild(_td);
+			if (isset(configformulaire[key]["phrase"][configPhrase]["required"])) {
+				// TODO - affiche les pre-requis
+			}
+			_table.appendChild(_tr);
+		}
+		
+		phase.appendChild(_table);
+		_frame.appendChild(phase);
+		
+		document.getElementById('pageIntent').appendChild(_frame);
 	}
 
 	function showForm(formId) {
@@ -173,11 +287,11 @@ sendVarToJS('varList', $varList);
 	}
 
 	function showPanel(formId, panelId) {
-		if (document.getElementById(formId + panelId).style.display == 'block') {
-			document.getElementById(formId + panelId).style.display = 'none';
-		} else {
-			document.getElementById(formId + panelId).style.display = 'block';
+		let listPage = document.getElementById(formId).querySelectorAll('.page');
+		for (let i = 0; i < listPage.length; i++) {
+			listPage[i].style.display = 'none';
 		}
+		document.getElementById(panelId + "-" + formId).style.display = 'block';
 	}
 
     function toggleInput(container,input) {
@@ -197,6 +311,12 @@ sendVarToJS('varList', $varList);
 	$('#bt_trainmodel').off().on('click',function() {
 		saveIntentsForm()
 	})
+
+	$('#bt_test').off().on('click',function() {
+		test()
+	})
+
+
 
 	function saveIntentsForm() {
 		let _formulaire = {};
@@ -243,7 +363,6 @@ sendVarToJS('varList', $varList);
 						}
 					break;
 				}
-				console.log("10")
 				_formulaire[configformulaire[key]["idForm"]]['module'].push({
 					"name": _module["name"],
 					"value": _moduleValue,
